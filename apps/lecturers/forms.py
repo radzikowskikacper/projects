d4415b41f9034b27fd92f3dd37ee21b5f0f2870c
Wiktor Franus <wiktor515.95@gmail.com>
2017-01-21 15:39:54 +0100

    [#156] Dodano autofocus na pole 'title' w ProjectForm

--- apps/lecturers/forms.py
+++ apps/lecturers/forms.py
@@ -1,5 +1,5 @@
 from django.core.exceptions import NON_FIELD_ERRORS
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput
 
 from projects_helper.apps.common.models import Project
 from django.utils.translation import ugettext_lazy as _
@@ -10,6 +10,9 @@ class ProjectForm(ModelForm):
         model = Project
         fields = ['title', 'description']
         required_css_class = 'required'
+        widgets = {
+            'title': TextInput(attrs={'autofocus': ''}),
+        }
         error_messages = {
             NON_FIELD_ERRORS: {
                 'unique_together': _("%(model_name)s's %(field_labels)s are not unique."),
