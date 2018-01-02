70ab42e2d393b902ee3ea3fffa8f77461f573ef3
Kacper Radzikowski <radzikowskikacper@gmail.com>
2018-01-02 01:24:11 +0100

    [#549] fixes

--- apps/students/views.py
+++ apps/students/views.py
@@ -323,7 +323,7 @@ def files(request, course_code, project_pk, file_id = None):
     elif request.method == 'POST':
         if file_id is None:
             File.objects.create(team = request.user.student.team(Course.objects.get(code = course_code)),
-                                filename = '', filedata = '',
+                                filename = ''.encode(), filedata = '',
                                 project = Project.objects.get(id = project_pk))
             return HttpResponse(status=200)
 
