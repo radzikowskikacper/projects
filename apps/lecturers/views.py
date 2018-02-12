feec86c4dc5a7b8a637f34baf7fbbba8ef95e535
Kacper Radzikowski <radzikowskikacper@gmail.com>
2018-02-12 22:05:26 +0100

    [#569] code fixes

--- apps/lecturers/views.py
+++ apps/lecturers/views.py
@@ -487,7 +487,7 @@ def assign_teams_to_projects(request, course_code):
 @user_passes_test(is_lecturer)
 @ensure_csrf_cookie
 def assign_all_teams(request, project_pk):
-    project = Project.objects.filter(
+    project = Project.objects.get(
         lecturer=request.user.lecturer, id = project_pk)
     project.assign_all_teams()
     messages.success(request, _(
